3006f4a1320933a0571d05f111360f271ad2aa65
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 10:06:55 +0000

    ProductGridItem.js: changed

--- src/components/ProductGridItem.js
+++ src/components/ProductGridItem.js
@@ -27,6 +27,7 @@ export default class ProductGridItem extends React.Component {
                             );
                         })())}
                     </h3></Link>
+                    <span className="product-grid__category"> {_.get(category_page, 'frontmatter.title', null)} </span>
                     
 
                     <Link to={withPrefix(_.get(product_page, 'url', null))}><h3 className="product-grid__title">{_.get(product_page, 'frontmatter.title', null)}</h3></Link>
