cdeeec8758a3aa63b701b700a3f38e8eb639a505
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 09:59:21 +0000

    ProductGridItem.js: changed

--- src/components/ProductGridItem.js
+++ src/components/ProductGridItem.js
@@ -20,10 +20,10 @@ export default class ProductGridItem extends React.Component {
                 </figure>
                 <div className="product-grid__definition">
                     <Link to={withPrefix(_.get(product_page, 'url', null))}><h3 className="product-grid__title">{_.get(product_page, 'frontmatter.title', null)}</h3></Link>
-                    {_.get(product_page, 'frontmatter.category', null) && ((() => {
+                    {_.get(product_page, 'frontmatter.category', null) && ((() => {<span className="product-grid__category"> {_.get(category_page, 'frontmatter.title', null)} </span>
                         let category_page = getPageByFilePath(this.props.pageContext.pages, _.get(category_page, 'frontmatter.category', null));
                         return (
-                            <span className="product-grid__category"> {_.get(category_page, 'frontmatter.title', null)} </span>
+                            
                         );
                     })())}
                     <span className="product-grid__price"> ${_.get(product_page, 'frontmatter.price', null)} </span>
